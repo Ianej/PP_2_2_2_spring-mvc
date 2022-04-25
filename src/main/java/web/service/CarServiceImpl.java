3ff3d8b0cdda2a48a984a1e2ee45a612d234cb35
Evgeniy Bondarev <ianej_1@mail.ru>
2022-04-25 20:08:05 +0300

    2.2.2 v2.1

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,12 @@
 package web.service;
 
+import web.dao.CarDao;
 import web.dao.CarDaoImpl;
 import web.model.Car;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarServiceImpl implements CarService{
-    private static final CarDaoImpl carDao = new CarDaoImpl();
+    private static final CarDao carDao = new CarDaoImpl();
 
     @Override
     public List<Car> getCarList(int count) {
